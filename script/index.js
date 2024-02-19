ec6ddb258d2fe45a3ab69c3b131e4d87a21c0464
mddolarmahmud <hemmeligdolar@gmail.com>
2024-02-19 09:22:49 +0600

    add button background color set function

--- script/index.js
+++ script/index.js
@@ -2,3 +2,33 @@ function toShowBuyTicket() {
 	const pParibahan = document.getElementById('p-paribahan');
 	pParibahan.classList.remove('hidden');
 }
+
+// function toSiteNoOneSelect() {
+// 	getSingleSit('toChange-color');
+// 	getSingleSit('secound');
+// }
+
+// function getSingleSit(elementId) {
+// 	const setBackgroundColor = document.getElementById(elementId);
+// 	setBackgroundColor.classList.add('bg-[#1DD100]');
+// }
+
+function toSiteNoOneSelect() {
+	const setBackgroundColor = document.getElementById('toChange-color');
+	setBackgroundColor.classList.add('bg-[#1DD100]');
+}
+
+function buttonTwo() {
+	const selectSecoundButton = document.getElementById('secound');
+	selectSecoundButton.classList.add('bg-[#1DD100]');
+}
+function toSelectThree() {
+	const slelectThirdButton = document.getElementById('third');
+	slelectThirdButton.classList.add('bg-[#1DD100]');
+}
+function toSelectFour() {
+	const selectFourButton = document.getElementById('four');
+	selectFourButton.classList.add('bg-[#1DD100]');
+}
+
+function toCountSelectSit() {}
